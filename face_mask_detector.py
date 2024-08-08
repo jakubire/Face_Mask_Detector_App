d72a2b7b384e7316293d86baed9b4568c551d375
Jacob A. Akubire <109191389+jakubire@users.noreply.github.com>
2024-08-08 13:22:14 -0400

    Update face_mask_detector.py

--- face_mask_detector.py
+++ face_mask_detector.py
@@ -1,23 +1,14 @@
 import streamlit as st
-import tensorflow 
+import tensorflow as tf
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Activation
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the model
 model = load_model('face_mask_cnn_model1.keras')
 
-def process_predict_image(image_path):
-    # Read the image
-    img = load_img(image_path, target_size=(128, 128))
-    # Display the image
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show()
-    
+def process_predict_image(img):
     # Normalize the image
     img = np.array(img) / 255.0
     
@@ -25,15 +16,8 @@ def process_predict_image(image_path):
     img_reshape = np.reshape(img, [1, 128, 128, 3])
 
     # Make a prediction
-    prediction = model.predict(img_reshape, verbose =0)
-    image_label = np.argmax(prediction)
-    
-    if image_label == 1:
-        #result = np.max(prediction)
-        return "The image is predicted to be wearing a face mask with probability"
-    else:
-        #result = np.max(prediction)
-        return "The image is predicted to be not wearing a face mask with probability"
+    prediction = model.predict(img_reshape, verbose=0)
+    return prediction
 
 # Streamlit app
 st.title('Face Mask Detector using CNN')
@@ -41,8 +25,16 @@ st.title('Face Mask Detector using CNN')
 # User input
 uploaded_file = st.file_uploader("Choose an image...", type=['png', 'jpeg', 'jpg'])
 if uploaded_file is not None:
-    # Preprocess the image and get the prediction
-    
-    st.write(f'**Prediction**: {process_predict_image(uploaded_file)}')
+    # Load and display the image
+    image = load_img(uploaded_file, target_size=(128, 128))
+    st.image(image, caption='Uploaded Image', use_column_width=True)
 
+    # Preprocess the image and get the prediction
+    prediction = process_predict_image(image)
+    prediction_prob = np.max(prediction)
+
+    if np.argmax(prediction) == 1:
+        st.write(f'*Prediction*: The uploaded image is predicted to be **wearing a face mask** with probability: {prediction_prob:.2f}')
+    else:
+        st.write(f'*Prediction*: The uploaded image is predicted to be **not wearing a face mask** with probability: {prediction_prob:.2f}')
         
